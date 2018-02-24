d4f3f49868fd68bdd14f2057b858ffadf02308e0
Bruce Shaw <battery4cid@gmail.com>
2018-02-24 15:32:36 -0500

    Cleaned up level-menu

--- src/level-menu.js
+++ src/level-menu.js
@@ -1,12 +1,23 @@
 (function() {
    var LevelMenu = function(game) {
       this.game = game;
-      //TODO: move to method
       // colors is actually the array of level pages
-      this.colors = ["0xac81bd", "0xff5050", "0xdab5ff", "0xb5ffda", "0xfffdd0", "0xcc0000", "0x54748b",
-            "0x4b0082", "0x80ab2f", "0xff784e", "0xe500db", "0x223c4a", "0x223c4a", "0xf1290e", "0x648080",
-            "0xbbc1c4", "0x6f98a2", "0x71717e"
-         ];
+      var colors = ["0xac81bd", "0xff5050", "0xdab5ff", "0xb5ffda", "0xfffdd0", "0xcc0000", "0x54748b",
+         "0x4b0082", "0x80ab2f", "0xff784e", "0xe500db", "0x223c4a", "0x223c4a", "0xf1290e", "0x648080",
+         "0xbbc1c4", "0x6f98a2", "0x71717e"
+      ];
+      var WORKING_WIDTH = TDG.GAME_WIDTH * .9;
+      var WORKING_HEIGHT = TDG.GAME_HEIGHT * .9;
+
+      this.getWorkingWidth = function() {
+         return WORKING_WIDTH;
+      }
+      this.getWorkingHeight = function() {
+         return WORKING_HEIGHT;
+      }
+      this.getLevelColors = function() {
+         return colors;
+      }
    };
 
    LevelMenu.prototype = {
@@ -24,50 +35,56 @@
          // space needed for title
          var titleSpace = TDG.GAME_HEIGHT * .1;
 
-         var WORKING_WIDTH = TDG.GAME_WIDTH * .9;
-         var WORKING_HEIGHT = TDG.GAME_HEIGHT * .9;
-         //TODO: fix this, don't have both here, again maybe do method
-         this.WORKING_WIDTH = WORKING_WIDTH;
-
-         var scaleRatio = (WORKING_WIDTH / (actualThumbWidth * columns)) * .60;
+         var scaleRatio = (this.getWorkingWidth() / (actualThumbWidth * columns)) * .60;
          var thumbWidth = (scaleRatio) * actualThumbWidth;
          var thumbHeight = (scaleRatio) * actualThumbWidth;
-         var spacingX = (WORKING_WIDTH - (columns * thumbWidth)) / (columns - 1) * .6;
-         var spacingY = ((WORKING_HEIGHT - titleSpace) - (rows * thumbHeight)) / (rows - 1 ) * .6;
+         var spacingX = (this.getWorkingWidth() - (columns * thumbWidth)) / (columns - 1) * .6;
+         var spacingY = ((this.getWorkingHeight() - titleSpace) - (rows * thumbHeight)) / (rows - 1) * .6;
 
          this.game.stage.backgroundColor = "#000044";
-         this.pageText = this.game.add.text(WORKING_WIDTH / 2, 20 * scaleRatio, "Select Level ( page 1 / " +
-            this.colors.length + ")", {
+         this.pageText = this.game.add.text(this.getWorkingWidth() / 2, 20 * scaleRatio,
+            "Select Level ( page 1 / " +
+            this.getLevelColors().length + ")", {
                font: (scaleRatio * 16) + "px Arial",
                fill: "#ffffff"
             })
          this.pageText.anchor.set(0.5);
 
-         this.scrollingMap = this.game.add.tileSprite(0, 0, this.colors.length * WORKING_WIDTH, WORKING_HEIGHT,
+         this.scrollingMap = this.game.add.tileSprite(0, 0, this.getLevelColors().length * this.getWorkingWidth(),
+            this.getWorkingHeight(),
             "transp");
          this.scrollingMap.inputEnabled = true;
          this.scrollingMap.input.enableDrag(false);
          this.scrollingMap.input.allowVerticalDrag = false;
-         this.scrollingMap.input.boundsRect = new Phaser.Rectangle(WORKING_WIDTH - this.scrollingMap.width,
-            WORKING_HEIGHT -
-            this.scrollingMap.height, this.scrollingMap.width * 2 - WORKING_WIDTH, this.scrollingMap.height *
-            2 - WORKING_HEIGHT);
+         this.scrollingMap.input.boundsRect = new Phaser.Rectangle(this.getWorkingWidth() - this.scrollingMap.width,
+            this.getWorkingHeight() -
+            this.scrollingMap.height, this.scrollingMap.width * 2 - this.getWorkingWidth(), this.scrollingMap
+            .height *
+            2 - this.getWorkingHeight());
          this.currentPage = 0;
 
          var rowLength = (thumbWidth * columns) + (spacingX * (columns - 1));
-         var leftMargin = (WORKING_WIDTH - rowLength) / 2;
+         var leftMargin = (this.getWorkingWidth() - rowLength) / 2;
          var colHeight = (thumbHeight * rows) + (spacingY * (rows - 1));
-         var topMargin = ((WORKING_HEIGHT - colHeight) / 2) + titleSpace;
+         var topMargin = ((this.getWorkingHeight() - colHeight) / 2) + titleSpace;
 
-         for (var k = 0; k < this.colors.length; k++) {
+         for (var k = 0; k < this.getLevelColors().length; k++) {
             for (var i = 0; i < columns; i++) {
                for (var j = 0; j < rows; j++) {
-                  
-                  var thumb = this.game.add.image(k * WORKING_WIDTH + leftMargin + i * (thumbWidth + spacingX),
+
+                  var thumb = this.game.add.image(k * this.getWorkingWidth() + leftMargin + i * (thumbWidth +
+                        spacingX),
                      topMargin + j * (thumbHeight + spacingY), "levelthumb");
-                  
-                  thumb.tint = this.colors[k];
-                  thumb.scale.setTo(1 * scaleRatio,1 * scaleRatio);
+
+                  thumb.levelNumber = k * (rows * columns) + j * columns + i;
+                  var levelText = this.game.add.text(0, 0, thumb.levelNumber, {
+                     font: "36px Arial",
+                     fill: "#000000"
+                  })
+                  thumb.addChild(levelText);
+
+                  thumb.tint = this.getLevelColors()[k];
+                  thumb.scale.setTo(1 * scaleRatio, 1 * scaleRatio);
                   this.scrollingMap.addChild(thumb);
                }
             }
@@ -78,14 +95,25 @@
             this.scrollingMap.currentPosition = this.scrollingMap.x;
          }, this);
 
-         this.scrollingMap.events.onDragStop.add(function() {
-            if (this.scrollingMap.startPosition - this.scrollingMap.x > WORKING_WIDTH / 8) {
-               this.changePage(1);
+         this.scrollingMap.events.onDragStop.add(function(event, pointer) {
+            if (this.scrollingMap.startPosition == this.scrollingMap.x) {
+               for (i = 0; i < this.scrollingMap.children.length; i++) {
+                  var bounds = this.scrollingMap.children[i].getBounds();
+                  if (bounds.contains(pointer.x, pointer.y)) {
+                     alert("Play level " + this.scrollingMap.children[i].levelNumber);
+                     break;
+                  }
+               }
             } else {
-               if (this.scrollingMap.startPosition - this.scrollingMap.x < -WORKING_WIDTH / 8) {
-                  this.changePage(-1);
+               if (this.scrollingMap.startPosition - this.scrollingMap.x > this.getWorkingWidth() / 8) {
+                  this.changePage(1);
                } else {
-                  this.changePage(0);
+                  if (this.scrollingMap.startPosition - this.scrollingMap.x < -this.getWorkingWidth() /
+                     8) {
+                     this.changePage(-1);
+                  } else {
+                     this.changePage(0);
+                  }
                }
             }
          }, this);
@@ -93,13 +121,13 @@
       changePage: function(page) {
          this.currentPage += page;
          this.pageText.text = "Select Level ( page " + (this.currentPage + 1).toString() + " / " +
-            this.colors.length + ")";
+            this.getLevelColors().length + ")";
          var tween = this.game.add.tween(this.scrollingMap).to({
-            x: this.currentPage * -this.WORKING_WIDTH
+            x: this.currentPage * -this.getWorkingWidth()
          }, 300, Phaser.Easing.Cubic.Out, true);
       },
       render: function() {
-       
+
       }
    };
 
